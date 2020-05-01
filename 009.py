c658c3a496221f327ea3c6c1256c988fc5ae5966
Keonho Lee <50159622+kunno97@users.noreply.github.com>
2020-05-01 14:47:44 -0500

    Create 009.py

---
+++ 009.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0: return False
+        s = str(x)
+        while len(s) > 1:
+            if s[0] != s[-1]: return False
+            s = s[1:-1]
+        return True
